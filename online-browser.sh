b622029c0b5193451caaa76b9756d3346b53ad8d
x011-al <x011pagkor@gmail.com>
2024-12-09 07:55:10 -0500

    Update online-browser.sh

--- online-browser.sh
+++ online-browser.sh
@@ -81,7 +81,7 @@ case $choice in
             -e PUID=1000 \
             -e PGID=1000 \
             -e TZ=Etc/UTC \
-            -p 3000:3000 \
+            -p 80:3000 \
             -p 3001:3001 \
             -v /chromium:/config \
             --shm-size="7gb" \
